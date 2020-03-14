bda436d19bd4abd0f3b2b5a4e604501a1c50dd59
rsharma <gaurnitai@rsharmas-MacBook-Pro.local>
2020-03-14 16:18:21 +0530

    Added new line of code

--- seleniumcode-v1.py
+++ seleniumcode-v1.py
@@ -12,3 +12,19 @@ driver.maximize_window()
 page_title = driver.title
 
 print(page_title)
+
+
+search_box = driver.find_element_by_id("twotabsearchtextbox")
+search_box.send_keys("Macbook Pro 15 inch")
+
+search_icon = driver.find_element_by_xpath("//input[@type='submit']")
+search_icon.click()
+sleep(3)
+
+expected_product_list = [
+    'Apple MacBook Pro (15-inch, Latest Model, 16GB RAM, 256GB Storage, 2.6GHz Intel Core i7) - Space Grey',
+    'New Apple MacBook Pro (13-inch, 8GB RAM, 128GB Storage, 1.4GHz Intel Core i5) - Space Grey']
+
+products_list = driver.find_elements_by_xpath(
+    "//div[@class='s-include-content-margin s-border-bottom']//a[@class='a-link-normal a-text-normal']")
+
